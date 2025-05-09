3269922655baf6978b7091eb3caae6f2c9aed87c
Manuel Rodriguez <cuentapersonal.0098@gmail.com>
2025-05-09 12:50:00 -0500

    app.js modificado para ser llamado por index.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,19 @@
 
-import { cors, express } from './src/bookstores/bookstores.js';
-import { addProductToList } from './src/routes/added-product-to-list/added-product-to-list.js';
-import { createProductDb } from './src/routes/create-product/create-product.js';
-import { deleteAllProducts } from './src/routes/delete-all-products/delete-all-products.js';
-import { deletebackupProduct } from './src/routes/delete-backup-product/delete-backup-product.js';
-import { deleteProductDB } from './src/routes/delete-product/delete-product.js';
-import { getCrossedOutProducts } from './src/routes/get-crossed-out-products/get-crossed-out-products.js';
-import { getProductsList } from './src/routes/products-added-to-the-list/products-added-to-the-list.js';
-import { routes } from './src/routes/routes.js';
-import { saveCrossedOutProducts } from './src/routes/save-crossed-out-products/save-crossed-out-products.js';
-import { searchProductDB } from './src/routes/search-product/search-product.js';
-import { updateProductList } from './src/routes/update-product-list/update-product-list.js';
-import { updateProductDB } from './src/routes/update-product/update-product.js';
-import { welcomePage } from './src/routes/welcome-page/welcome-page.js';
+import { cors, express } from './bookstores/bookstores.js';
+import { addProductToList } from './routes/added-product-to-list/added-product-to-list.js';
+import { createProductDb } from './routes/create-product/create-product.js';
+import { deleteAllProducts } from './routes/delete-all-products/delete-all-products.js';
+import { deletebackupProduct } from './routes/delete-backup-product/delete-backup-product.js';
+import { deleteProductDB } from './routes/delete-product/delete-product.js';
+import { getCrossedOutProducts } from './routes/get-crossed-out-products/get-crossed-out-products.js';
+import { getProductsList } from './routes/products-added-to-the-list/products-added-to-the-list.js';
+import { routes } from './routes/routes.js';
+import { saveCrossedOutProducts } from './routes/save-crossed-out-products/save-crossed-out-products.js';
+import { searchProductDB } from './routes/search-product/search-product.js';
+import { updateProductList } from './routes/update-product-list/update-product-list.js';
+import { updateProductDB } from './routes/update-product/update-product.js';
+import { welcomePage } from './routes/welcome-page/welcome-page.js';
 
-
-const port = process.env.PORT;
 
 const app = express();
 
@@ -37,9 +35,8 @@ app.use( '/api', getCrossedOutProducts);
 app.use( '/api', updateProductList );
 app.use( '/api', routes );
 
-( ( ) => {
 
-    app.listen(port, ()=>{
-        console.log(`Servidor corriendo en el puerto ${port} `);
-    });
-})(port);
+
+export {
+    app
+}
