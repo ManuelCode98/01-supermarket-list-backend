eda98bc2b1754084c5cf75de65f3b2cd29f36dff
Manuel Rodriguez <cuentapersonal.0098@gmail.com>
2025-05-09 15:51:35 -0500

    \routes\routes.js: propiedades para hacer las peticiones organizadas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,16 +4,16 @@ import { controllers } from '../controllers/controllers.js';
 const routes = Router();
 
 routes.post('/create-product', controllers.createProductDb );
-routes.post('/delete-product/:id', controllers.deleteProductDB );
-routes.post('/update-product/:id', controllers.updateProductDB );
+routes.delete('/delete-product/:id', controllers.deleteProductDB );
+routes.put('/update-product/:id', controllers.updateProductDB );
 routes.post('/search-product', controllers.searchProductDB );
 routes.post('/add-product-to-list', controllers.addProductToList );
-routes.post('/show-products', controllers.getProductsList );
-routes.post('/delete-all-products', controllers.deleteAllProducts );
+routes.get('/show-products', controllers.getProductsList );
+routes.delete('/delete-all-products', controllers.deleteAllProducts );
 routes.post('/save-crossed-out-products', controllers.saveCrossedOutProducts );
-routes.post('/delete-backup-product/:id', controllers.deletebackupProduct );
-routes.post('/get-crossed-out-products', controllers.getCrossedOutProducts );
-routes.post('/update-product-list/:id', controllers.updateProductList  );
+routes.delete('/delete-backup-product/:id', controllers.deletebackupProduct );
+routes.get('/get-crossed-out-products', controllers.getCrossedOutProducts );
+routes.put('/update-product-list/:id', controllers.updateProductList  );
 routes.post('/upload-rendered-image', controllers.set_rendered_image )
 
 
