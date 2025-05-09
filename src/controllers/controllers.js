2c1dd24c76e53bb61262b77fb93b04d0688b179d
Manuel Rodriguez <cuentapersonal.0098@gmail.com>
2025-05-09 13:16:05 -0500

    routes.js Todas las rutas fueron modificadas ahora se llaman desde el controlador

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,8 +1,17 @@
+import { createProductDb } from "../routes/create-product/create-product.js";
 import { uploadRenderedImage } from "../services/upload-rendered-image.js";
 
 
 
 const controllers ={ 
+
+    createProductDb: ()=>{
+        try {
+            createProductDb( req, res );       
+        } catch (error) {
+             res.status(500).json({ error: error.message })
+        }
+    } ,
 
     set_rendered_image : (req, res)=>{
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,17 @@ import { controllers } from '../controllers/controllers.js';
 
 const routes = Router();
 
+routes.post('/create-product', controllers.createProductDb );
+routes.post('/delete-product/:id', controllers.deleteProductDB );
+routes.post('/update-product/:id', controllers.updateProductDB );
+routes.post('/search-product', controllers.searchProductDB );
+routes.post('/add-product-to-list', controllers.addProductToList );
+routes.post('/show-products', controllers.getProductsList );
+routes.post('/delete-all-products', controllers.deleteAllProducts );
+routes.post('/save-crossed-out-products', controllers.saveCrossedOutProducts );
+routes.post('/delete-backup-product/:id', controllers.deletebackupProduct );
+routes.post('/get-crossed-out-products', controllers.getCrossedOutProducts );
+routes.post('/update-product-list/:id', controllers.updateProductList  );
 routes.post('/upload-rendered-image', controllers.set_rendered_image )
 
 
