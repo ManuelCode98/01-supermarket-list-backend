cd3a1af1a6b341692a2c130c5c1846f8a758fb1b
ManuelCode98 <162215855+ManuelCode98@users.noreply.github.com>
2025-05-14 07:29:51 -0500

    Delete migrations directory

--- migrations/20250310003945_/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "CreateProduct" (
-    "id" SERIAL NOT NULL,
-    "product_name" TEXT NOT NULL,
-    "product_photo" TEXT NOT NULL,
-
-    CONSTRAINT "CreateProduct_pkey" PRIMARY KEY ("id")
-);
